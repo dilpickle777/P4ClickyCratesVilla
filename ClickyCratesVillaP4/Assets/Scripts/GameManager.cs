4d8174f882493508809f7e9ccd99ceedfba3feb6
dilpickle777 <727696@student.dusd.net>
2025-05-22 11:42:59 -0700

    ADDED RESTART
    added restaart buttom

--- ClickyCratesVillaP4/Assets/Scripts/GameManager.cs
+++ ClickyCratesVillaP4/Assets/Scripts/GameManager.cs
@@ -2,11 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
     public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
+    public Button restartButton;
+    public bool isGameActive;
     public int score;
     private float spawnRate = 1.0f;
     
@@ -14,8 +19,9 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(SpawnTarget());
+        isGameActive = true;
         score = 0;
+        StartCoroutine(SpawnTarget());
         UpdateScore(0);
     }
 
@@ -27,7 +33,7 @@ public class GameManager : MonoBehaviour
 
     IEnumerator SpawnTarget()
     {
-        while (true)
+        while (isGameActive)
         {
             yield return new WaitForSeconds(spawnRate);
             int index = Random.Range(0, targets.Count);
@@ -41,4 +47,16 @@ public class GameManager : MonoBehaviour
              score += scoreToAdd;
              scoreText.text = "Score" + score;
            }
+    public void GameOver()
+    {
+        restartButton.gameObject.SetActive(true);
+        gameOverText.gameObject.SetActive(true);
+        isGameActive = false;
+    }
+    
+
+    public void RestartGame()
+    { 
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
 }
