4e4ce5d83e75803a11e3e45034d724bf9c6979bd
dilpickle777 <727696@student.dusd.net>
2025-06-02 12:38:04 -0700

    DONEZO

--- ClickyCratesVillaP4/Assets/Scripts/GameManager.cs
+++ ClickyCratesVillaP4/Assets/Scripts/GameManager.cs
@@ -9,13 +9,17 @@ public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
     public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI livesText;
+    private int lives;
     public TextMeshProUGUI gameOverText;
+    public GameObject pauseScreen;
+    private bool paused;
     public Button restartButton;
     public GameObject titleScreen;
     public bool isGameActive;
     public int score;
     private float spawnRate = 1.0f;
-    
+
 
     // Start is called before the first frame update
     void Start()
@@ -26,7 +30,11 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        //Check if the user has pressed the P key
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            ChangePaused();
+        }
     }
 
     IEnumerator SpawnTarget()
@@ -41,25 +49,34 @@ public class GameManager : MonoBehaviour
     }
 
     public void UpdateScore(int scoreToAdd)
-           {
-             score += scoreToAdd;
-             scoreText.text = "Score" + score;
-           }
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score" + score;
+    }
+    public void UpdateLives(int livesToChange)
+    {
+        lives += livesToChange;
+        livesText.text = "Lives: " + lives;
+        if (lives <= 0)
+        {
+            GameOver();
+        }
+    }
     public void GameOver()
     {
         restartButton.gameObject.SetActive(true);
         gameOverText.gameObject.SetActive(true);
         isGameActive = false;
     }
-    
+
 
     public void RestartGame()
-    { 
+    {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
     public void StartGame(int difficulty)
-    { 
+    {
         isGameActive = true;
         score = 0;
         spawnRate /= difficulty;
@@ -67,6 +84,24 @@ public class GameManager : MonoBehaviour
         StartCoroutine(SpawnTarget());
         UpdateScore(0);
 
+
         titleScreen.gameObject.SetActive(false);
+        UpdateLives(3);
     }
-}
+    void ChangePaused()
+    {
+        if (!paused)
+        {
+            paused = true;
+            pauseScreen.SetActive(true);
+            Time.timeScale = 0;
+
+        }
+        else
+        {
+            paused = false;
+            pauseScreen.SetActive(false);
+            Time.timeScale = 1;
+        }
+
+ }   }
